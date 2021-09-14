3ca5d05abaa117d680931db295a4367f446b875c
Vinicius Martins <vm3090474@gmail.com>
2021-09-14 13:22:58 -0300

    Conhecendo arrays

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -13,6 +13,15 @@ namespace ByteBank.SistemaAgencia
         static void Main(string[] args)
         {
             
+            int[] idades = new int[5];
+
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+            idades[3] = 50;
+            idades[4] = 28;
+
+            Console.WriteLine(idades[4]);
 
         }
     }
